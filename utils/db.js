5c915ad5b547fc41b03a56d9e5f623315491b3b5
Pranav1326 <visavadiapa@gmail.com>
2024-11-03 02:53:18 +0530

    Upgraded to Mongodb Atlas

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
-mongoose.connect(`mongodb://localhost:27017`, { dbName: 'NAS' })
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.bmhdr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, { dbName: 'NAS' })
 .then(() => console.log(`MongoDB Connected!`))
 .catch(err => console.log(err));
 
